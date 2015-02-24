410c3194181973cafe456c467f7d05c26b1369c8
Andonov91 <andonovv91@gmail.com>
2015-02-24 17:52:08 +0200

    Added Bird and Boundaries classes

---
+++ Naga/FlappyBird/Boundaries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBird
+{
+    class Boundaries
+    {
+      public int leftX;
+      public   int leftY;
+      public    int rightX;
+      public  int rightY;
+      public int height;
+    }
+}
