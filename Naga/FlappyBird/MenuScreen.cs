4cf61175dc6ca320078d738a84a61d962ede5422
Bruno <t1m2n3t4@yahoo.com>
2015-03-03 13:58:05 +0200

    Forgot to add the MenuScreen.cs

---
+++ Naga/FlappyBird/MenuScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBird
+{
+    class MenuScreen
+    {
+        static public ConsoleKeyInfo Menu()
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\n\n\n");
+            Console.Write("{0,95}", "███╗   ███╗███████╗███╗   ██╗██╗   ██╗\n");
+            Console.Write("{0,95}", "████╗ ████║██╔════╝████╗  ██║██║   ██║\n");
+            Console.Write("{0,95}", "██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║\n");
+            Console.Write("{0,95}", "██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║\n");
+            Console.Write("{0,95}", "██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝\n");
+            Console.Write("{0,95}", "╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ \n");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 7, Console.WindowHeight / 2 - 7);
+            Console.WriteLine("1. START GAME");
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 7, Console.WindowHeight / 2 - 6);
+            Console.WriteLine("2. SCORE");
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 7, Console.WindowHeight / 2 - 5);
+            Console.WriteLine("3. EXIT");
+            var result = Console.ReadKey(true);
+            return result;
+
+        }
+    }
+}
